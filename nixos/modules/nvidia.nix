31a6d73d4094f37ae8d8497b8032ab94d81ea3a7
clement <clement.jvdm@gmail.com>
2024-11-09 00:02:16 +0100

    added nvidia prime config (work in progress)

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
 let 
   cfg = config.setup.nvidia;
@@ -9,47 +9,63 @@ in
     nvidia = {
       enable = lib.mkEnableOption "NVIDIA driver configuration";
 
-      prime = lib.mkOption {
+      laptop = lib.mkOption {
         type = lib.types.bool;
         default = false;
-        description = "Whether to configure PRIME (needed for laptops)";
+        description = "Configures prime if laptop";
       };
+
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    # Enable OpenGL
-    hardware.opengl = {
-      enable = true;
-    };
+  config = (lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      # Enable OpenGL
+      hardware.opengl = {
+        enable = true;
+      };
 
-    # Load nvidia driver for Xorg and Wayland
-    services.xserver.videoDrivers = ["nvidia"];
+      # Load nvidia driver for Xorg and Wayland
+      services.xserver.videoDrivers = ["nvidia"];
 
-    hardware.nvidia = {
+      hardware.nvidia = {
 
-      # Modesetting is required.
-      modesetting.enable = true;
+        # Modesetting is required.
+        modesetting.enable = true;
 
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-      # Enable this if you have graphical corruption issues or application crashes after waking
-      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-      # of just the bare essentials.
-      powerManagement.enable = false;
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+        # of just the bare essentials.
+        powerManagement.enable = false;
 
-      # Fine-grained power management. Turns off GPU when not in use.
-      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-      powerManagement.finegrained = false;
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
 
-      # use open source drivers
-      open = false;
+        # use open source drivers
+        open = false;
 
-      # Enable the Nvidia settings menu,
-      # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
 
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
-  };
+        # Optionally, you may need to select the appropriate driver version for your specific GPU.
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+      };
+    }
+
+    (lib.mkIf cfg.laptop {
+	     hardware.nvidia.prime = {
+	       offload = {
+	      enable = true;
+	      enableOffloadCmd = true;
+	     };
+	       # Make sure to use the correct Bus ID values for your system!
+	       amdgpuBusId = "PCI:1:0:0"; # TODO: busID setup options
+	       nvidiaBusId = "PCI:6:0:0";
+	    };
+    })
+
+  ]));
 }
