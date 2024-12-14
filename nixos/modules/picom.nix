0e38a5d1dd5a1255668add57ea8dffead52a726b
clement <clement.jvdm@gmail.com>
2024-12-14 17:50:44 +0100

    added picom module

--- nixos/modules/picom.nix
+++ nixos/modules/picom.nix
@@ -1,3 +1,22 @@
+{ lib, config, ... }:
+let
+  cfg = config.setup.picom;
+in
 {
-  services.picom.enable = true;
+  options.setup.picom.enable = lib.mkEnableOption "Picom module";
+  config = ( lib.mkIf cfg.enable {
+    services.picom = {
+      enable = true;
+      backend = "glx";
+      shadow = true;
+      shadowOpacity = 0.9;
+      settings = {
+        blur = {
+          method = "gaussian";
+          size = 10;
+          deviation = 5.0;
+        };
+      };
+    };
+  });
 }
--- setup.nix
+++ setup.nix
@@ -2,13 +2,14 @@
   setup = {
     
     pipewire.enable = true;
-    steam.enable = true;
+    steam.enable = false;
     xfce.enable = true;
     cinnamon.enable = false;
     tlp.enable = true;
     nixvim.enable = true;
     bluetooth.enable = true;
     nvidia-laptop.enable = true;
+    picom.enable = true;
 
   };
 }
