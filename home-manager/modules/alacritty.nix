c12a676f76db250d7fcbb9355db04e893fbd590b
clement <clement.jvdm@gmail.com>
2024-12-14 17:51:14 +0100

    alacritty.nix: improved config

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -1,6 +1,23 @@
 {
   programs.alacritty = {
     enable = true;
-    settings.font.size = 9;
+    settings = {
+      font = {
+        size = 9;
+        normal.family = "FiraCode Nerd Font Mono";
+      };
+
+      env = {
+        TERM = "xterm-256color";
+      };
+
+      window = {
+        decorations = "none";
+        padding.x = 5;
+        padding.y = 10;
+        opacity = 0.8;
+        blur = true;
+      };
+    };
   };
 }
