fbd562876364e0f37c0cd62318fd4966595e5961
clement <clement.jvdm@gmail.com>
2024-12-15 23:09:31 +0100

    xorg.nix: remapped caps lock to ctrl

--- nixos/modules/xorg.nix
+++ nixos/modules/xorg.nix
@@ -7,7 +7,8 @@ in
   config = (lib.mkIf cfg.enable {
     services.xserver.enable = true;
     services.xserver.xkb.layout = "us";  # configure X11 keymap
-    services.xserver.xkb.options = "eurosign:e,caps:escape";
+    # services.xserver.xkb.options = "eurosign:e,caps:escape"; # remap caps to esc
+    services.xserver.xkb.options = "ctrl:swapcaps"; # remap caps to ctrl
     environment.systemPackages = with pkgs; [ xclip ];
   });
 }
