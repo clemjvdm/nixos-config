a374e59e23cfe7fcaa318566b80a84038c179c02
clement <clement.jvdm@gmail.com>
2024-12-18 16:03:13 +0100

    added description for all settings

--- setup.nix
+++ setup.nix
@@ -1,16 +1,43 @@
 {
   setup = {
-    
-    pipewire.enable = true;
-    steam.enable = false;
+   
+    # Desktop environment. Only one of these can
+    # be enabled at a time. 
+    # TODO: make a "desktop-environment" option
     xfce.enable = true;
     cinnamon.enable = false;
-    tlp.enable = true;
-    nixvim.enable = true;
-    bluetooth.enable = true;
-    nvidia-laptop.enable = true;
-    nvidia-laptop.mode = "prime-sync"; # prime-offload | prime-sync
+    
+    # A window compositor which allows effects like
+    # transparent windows with blurred backgrounds.
     picom.enable = true;
+    
+    # Provides a tools for video and audio playback
+    # to the system.
+    pipewire.enable = true;
+
+    # The Steam game store + options to make games run well
+    steam.enable = false;
+
+    # A custom neovim configuration, configured through
+    # nixvim.
+    # TODO: Let user define custom nixvim config here
+    nixvim.enable = true;
+
+    # Enabling bluetooth
+    bluetooth.enable = true;
+
+    
+    # Battery optimization, only necessary for laptops
+    tlp.enable = true;
+
+    # Enabling the nvidia-laptop module makes both
+    # prime-offload and prime-sync modes available as specializations
+    # (you can switch to them easily on boot). The mode option
+    # just changes the default.
+    nvidia-laptop = {
+      enable = true;
+      mode = "prime-offload"; # "prime-offload" | "prime-sync"
+    };
 
   };
 }
